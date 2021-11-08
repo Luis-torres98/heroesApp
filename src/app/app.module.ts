7660f6552f46a0aada618d413392edfeeb108c13
ltorres98 <luiseduardotorresfigueroa@gmail.com>
2021-11-08 17:23:17 -0500

    U

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPagesComponent } from './shared/error-pages/error-pages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPagesComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+
+@Component({
+	selector: 'app-listado',
+	templateUrl: './listado.component.html',
+	styleUrls: ['./listado.component.scss']
+})
+export class ListadoComponent implements OnInit {
+
+	heroes : Heroe[] = []
+
+	constructor(private _heroesSrv: HeroesService) { }
+
+	ngOnInit() {
+		this._heroesSrv.getHeroes().subscribe(resp => {
+			this.heroes = resp
+
+		})
+	}
+
+}
+
+
+
